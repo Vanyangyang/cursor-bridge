258bdcd6594047df91b7bc6389ff7ff8f4a9012b
cc <2226062736@qq.com>
2026-08-24 23:37:05 +0800

    release: grok build supervisor 0.3.5

--- plugins/grok-build-supervisor/dist/grok-build-supervisor.mjs
+++ plugins/grok-build-supervisor/dist/grok-build-supervisor.mjs
@@ -35268,7 +35268,7 @@ var SupervisorClient = class {
 // plugins/grok-build-supervisor/scripts/server.mjs
 var supervisor = new SupervisorClient();
 process.chdir(supervisor.paths.stateRoot);
-var server = new McpServer({ name: "grok-build-supervisor", version: "0.3.4" });
+var server = new McpServer({ name: "grok-build-supervisor", version: "0.3.5" });
 function toolResult(data, isError = false) {
   return {
     content: [{ type: "text", text: JSON.stringify(data) }],
